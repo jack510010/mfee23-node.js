7256ae9179ce4164796138ccf16da1eb773f73ed
Albert <jack25512909@gmail.com>
2022-01-25 10:57:59 +0800

    副檔名可以不寫

--- practice/func02.js
+++ practice/func02.js
@@ -1,4 +1,4 @@
 //const f1 = require('./func01');  這個寫法ok
-const f1 = require(__dirname + '/func01');  // 這個寫法也ok
+const f1 = require(__dirname + '/func01');  // 這個寫法也ok   副檔名'/func01.js' 那個.js可以不寫， 除非遇到檔名一樣副檔名不一樣的狀況
 
 console.log('func02', f1(8));
