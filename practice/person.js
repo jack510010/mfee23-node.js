bc9f7bb1e7f1bc62e99b37af66f217b266daae8d
Albert <jack25512909@gmail.com>
2022-01-25 10:38:47 +0800

    json

---
+++ practice/person.js
@@ -0,0 +1,20 @@
+class person{
+    constructor(name = 'noname', age = 0){
+        this.name = name,
+        this.age = age;
+    }
+    toJSON(){
+        return {
+            name: this.name,
+            age: this.age
+        }
+    }
+    sayHello(){
+        return `Hello, my name is ${this.name}.`;
+    }
+}
+const p1 = new person('Bill', 23);
+
+console.log(p1.sayHello());
+console.log(JSON.stringify(p1.toJSON()));
+console.log(JSON.stringify(p1));
