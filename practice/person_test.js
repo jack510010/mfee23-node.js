81d238c4fa4b9451fa8046db0f322b27d52d5881
Albert <jack25512909@gmail.com>
2022-01-25 14:20:18 +0800

    console.log 測試

--- practice/person_test.js
+++ practice/person_test.js
@@ -6,6 +6,7 @@ const p2 = new obj.person('Jack', 30);
 const p3 = new person('David',30);
 
 console.log(p2);
+console.log('Hi, ', p2.sayHello());
 console.log(p3);
 console.log(obj.f3(3));
 console.log(obj.person === person);
