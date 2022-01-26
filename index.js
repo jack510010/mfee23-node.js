35ebb8275936c6e8ec1e55b9301fa5976aa7bd5f
Albert <jack25512909@gmail.com>
2022-01-26 11:07:48 +0800

    自訂404網頁 還有 process.env.NODE_ENV

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
@@ -9,7 +11,17 @@ app.get('/', (req, res)=>{
 });
 
 
+// 所有路由的後面 res.status(404)
+app.use((req, res) => {
+    //res.type('text/plain'); // 這個寫入的話會變成純文字，包含標籤都變文字
+    res.status(404).send(`<h2>404 - 找不到網頁，走錯路了</h2>`)
+});
 
-app.listen(3000, ()=>{
-    console.log('server started');
+const port = process.env.PORT || 3001;
+
+
+console.log(process.env.NODE_ENV);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+app.listen(port, ()=>{
+    console.log(`啟動: ${port} -`, new Date());
 })
