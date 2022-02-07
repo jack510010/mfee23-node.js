e4f71c25a212a28f45b4adf124b213d737ab9038
Albert <jack25512909@gmail.com>
2022-02-07 14:55:05 +0800

    routes 路由模組化

---
+++ routes/admin2.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// 路由模組化
+const router = express.Router();
+
+router.get('/admin2/:p1?/:p2?', (req, res) => {
+    res.json({
+        params: req.params,
+        url: req.url,
+        baseUrl: req.baseUrl,
+        originalUrl: req.originalUrl
+    });
+});
+
+module.exports = router;  // 匯出router， 原則上這就是一個middleware。
