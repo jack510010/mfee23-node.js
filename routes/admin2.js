88aa49bc4c56668bdb2012fb6dc683151541742a
Albert <jack25512909@gmail.com>
2022-02-07 16:04:54 +0800

    自定義 middleware ， 會對到index.js檔案的自定義 middleware   測試用：忽略

--- routes/admin2.js
+++ routes/admin2.js
@@ -4,11 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/apple/:p1?/:p2?', (req, res) => {
+    res.locals.aaa += ' sexy'   // 自定義 middleware ， 會對到index.js檔案的自定義 middleware   測試用：忽略
     res.json({
         params: req.params,
         url: req.url,
         baseUrl: req.baseUrl,
-        originalUrl: req.originalUrl
+        originalUrl: req.originalUrl,
+        localsAAA: res.locals.aaa   // 自定義 middleware ， 會對到index.js檔案的自定義 middleware  測試用：忽略
     });
 });
 
