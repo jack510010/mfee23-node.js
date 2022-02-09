6f77aa52ddf8418a6f8e5b657f8d035e5ba66646
Albert <jack25512909@gmail.com>
2022-02-09 11:46:38 +0800

    製作搜尋功能

--- routes/address-book.js
+++ routes/address-book.js
@@ -11,30 +11,43 @@ async function getListData(req, res) {
         return res.redirect('/address-book/list');
     }
 
+    const conditions = {};  // 傳到 ejs 的條件
+    
+    let search = req.query.search ? req.query.search : '';
+    search = search.trim();  // 去掉頭尾空白
+    let sqlWhere = ' WHERE 1 ';
+    if(search){
+        sqlWhere += ` AND name LIKE ${db.escape('%'+search+'%')} `;
+        conditions.search = search;
+    }
+
+
+
     const output = {
         perPage,
         page,
         totalRows: 0,
         totalPages: 0,
-        rows: []
+        rows: [],
+        conditions
     };
 
-    const totalSql = `SELECT COUNT(1) num FROM address_book`;
+    const totalSql = `SELECT COUNT(1) num FROM address_book ${sqlWhere} `;
+    // return res.send(t_sql); // 除錯用
 
     const [result1] = await db.query(totalSql);
 
     const totalRows = result1[0].num;
 
-    let totalPages = 0;
 
     if(totalRows){
-        output.totalPages = Math.ceil(totalRows/totalPages);
+        output.totalPages = Math.ceil(totalRows/perPage);
         output.totalRows = totalRows;
         if(page > output.totalPages){
-            return res.redirect(`/address-book/list?page=${totalPages}`)
+            return res.redirect(`/address-book/list?page=${output.totalPages}`)
         }
 
-        const sql = `SELECT * FROM address_book LIMIT ${perPage * (page - 1)}, ${perPage}`;
+        const sql = `SELECT * FROM address_book ${sqlWhere} ORDER BY sid DESC LIMIT ${perPage * (page - 1)}, ${perPage} `;
         const [result2] = await db.query(sql);
         result2.forEach(el => el.birthday = res.locals.dateToString(el.birthday));
         output.rows = result2;
@@ -47,13 +60,13 @@ async function getListData(req, res) {
 router.get('/list', async (req, res)=>{
     
     res.render('address-book/list', await getListData(req, res));
-   
+
 });
 
 router.get('/api/list', async (req, res)=>{
     
     res.json(await getListData(req, res));
-   
+
 });
 
 
